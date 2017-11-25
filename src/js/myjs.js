6f53272b29cbf4fb380706308a1c33a730caf8c5
Mousa Saleh <mousasaleh@MacBook-Pro-alkhas-b-Mousa.local>
2017-11-26 00:19:44 +0100

    By Mousa

--- src/js/myjs.js
+++ src/js/myjs.js
@@ -2,4 +2,8 @@ $(document).ready(function(){
   $('.logo').on('click',function(){
       window.location.href='index.html';
   });
+  var location = window.location.href;
+  if (location = 'index.html') {
+    $('#home').css({'background-color':'#fff','color':'#000'});
+  }
 });
